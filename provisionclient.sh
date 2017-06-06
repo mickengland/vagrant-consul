41aa055214e27f2f81d9b501f84519d37f02f495
Mick England <mick@robinpowered.com>
2017-06-06 09:27:48 -0400

    Added vault to client

--- provisionclient.sh
+++ provisionclient.sh
@@ -2,6 +2,7 @@
 
 # Set variables
 consul_version=0.8.3
+vault_version=0.7.2
 export DEBIAN_FRONTEND=noninteractive
 
 # Upgrade packages and install utilities
@@ -12,6 +13,7 @@ apt-get install -y unzip
 # Copy the systemd scripts
 cp /vagrant/consul.conf /etc/init/consul.conf
 cp /vagrant/consul.service /etc/systemd/system/consul.service
+cp /vagrant/vault.service /etc/systemd/system/vault.service
 
 # Get the consul binaries
 cd /usr/local/bin
@@ -30,3 +32,22 @@ cp $1 /etc/consul.d/config.json
 # Enable and Start Consul with UI
 sudo systemctl enable consul.service
 sudo service consul start
+
+# Copy vault confi
+mkdir -p /etc/vault.d
+cp /vagrant/vault.hcl /etc/vault.d/vault.hcl
+
+# Get latest vault
+cd /usr/local/bin
+wget -q https://releases.hashicorp.com/vault/${vault_version}/vault_${vault_version}_linux_amd64.zip
+
+unzip *.zip
+rm *.zip
+
+# Enable and Start Vault 
+sudo systemctl enable vault.service
+sudo service vault start
+
+# Initialize the Vault
+export VAULT_ADDR='http://127.0.0.1:8200'
+vault init
